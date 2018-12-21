a435fccf52fb1d1441083427d1e7067a776b88dd
Vinh Ngu <Vinh Ngu>
2018-12-21 18:49:56 +0100

    Add date attribute.

--- src/main/java/file/File.java
+++ src/main/java/file/File.java
@@ -1,7 +1,15 @@
 package file;
 
+import java.util.Date;
 import java.util.UUID;
 
 public class File {
     private UUID _uuid;
+    private Date _created_at;
+    private Date _updated_at;
+
+    public File() {
+        _uuid = UUID.randomUUID();
+        _created_at = new Date();
+    }
 }
