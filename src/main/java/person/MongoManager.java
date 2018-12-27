c3d25c3fcbecac5f6e10f9a3a7465c066da44281
vngu <vinh-ngu@hotmail.com>
2018-12-27 05:56:30 +0100

    Update to newer java-mongo driver.

--- src/main/java/person/MongoManager.java
+++ src/main/java/person/MongoManager.java
@@ -4,20 +4,22 @@ import com.mongodb.DB;
 import com.mongodb.DBCollection;
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
 
 public class MongoManager {
 
     private static MongoManager instance;
-    private DB _db;
-    private DBCollection _patient_collection;
-    private DBCollection _nurse_collection;
-    private MongoClient _mongoclient;
+    private final MongoDatabase _db;
+    private final MongoCollection _patient_collection;
+    private final MongoCollection _nurse_collection;
+    private final MongoClient _mongoclient;
 
-    public DBCollection getPatientCollection() {
+    public MongoCollection getPatientCollection() {
         return _patient_collection;
     }
 
-    public DBCollection getNurseCollection() {
+    public MongoCollection getNurseCollection() {
         return _nurse_collection;
     }
 
@@ -25,10 +27,11 @@ public class MongoManager {
         String mongo_uri = System.getenv("MONGO_DB_URL");
         System.out.println(mongo_uri);
         _mongoclient = new MongoClient(new MongoClientURI(mongo_uri));
-        _db = (DB) _mongoclient.getDatabase("curilab");
+        _db = _mongoclient.getDatabase("curilab");
         _patient_collection = _db.getCollection("patients");
         _nurse_collection = _db.getCollection("nurses");
     }
+
 
     public static MongoManager getInstance() {
         if (MongoManager.instance == null) {
--- src/test/java/person/PersonManagerTest.java
+++ src/test/java/person/PersonManagerTest.java
@@ -1,47 +1,33 @@
 package person;
 
 import com.mongodb.BasicDBObject;
+import exception.PatientAlreadyExistsException;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import java.util.UUID;
 
 import static org.junit.Assert.*;
 
 public class PersonManagerTest {
-
     @Test
     public void getPatient() {
     }
 
-    @Test
-    public void addPatient() {
+
+    @Test(expected = PatientAlreadyExistsException.class)
+    public void addPatient() throws PatientAlreadyExistsException {
         UUID uuid1 = UUID.fromString("866f74aa-0472-40d8-99b0-799391a06e3b");
         UUID uuid2 = UUID.fromString("44ff17a3-1d91-4382-803e-80db767fcdd3");
-
-
+        "".length();
         // Add patient 1
         Patient patient1 = new Patient("Patient1");
         patient1.setUuid(uuid1);
+        assertFalse(PersonManager.getInstance().patientExists(patient1.getUuid()));
         PersonManager.getInstance().addPatient(patient1);
-        int count = PersonManager.getInstance().countPatient(new BasicDBObject("uuid", uuid1.toString()));
-        assertEquals(1, count);
-
-        // Add patient 2 with same uuid as patient1.
-        Patient patient2 = new Patient("Patient2");
-        patient2.setUuid(uuid1);
-        PersonManager.getInstance().addPatient(patient2);
-        count = PersonManager.getInstance().countPatient(new BasicDBObject("uuid", uuid1.toString()));
-        assertEquals(1, count);
-
-        // Add patient 3 with different id.
-        Patient patient3 = new Patient("Patient3");
-        patient3.setUuid(uuid2);
-        PersonManager.getInstance().addPatient(patient3);
-
-        count = PersonManager.getInstance().countPatient(new BasicDBObject("uuid", uuid1.toString()));
-        assertEquals(1, count);
-        count = PersonManager.getInstance().countPatient(new BasicDBObject("uuid", uuid2.toString()));
-        assertEquals(1, count);
+        assertTrue(PersonManager.getInstance().patientExists(patient1.getUuid()));
+        PersonManager.getInstance().addPatient(patient1);
     }
 
     @Test
@@ -57,30 +43,12 @@ public class PersonManagerTest {
         UUID uuid1 = UUID.fromString("866f74aa-0472-40d8-99b0-799391a06e3b");
         UUID uuid2 = UUID.fromString("44ff17a3-1d91-4382-803e-80db767fcdd3");
 
-
-        // Add patient 1
-        Nurse patient1 = new Nurse("Patient1");
-        patient1.setUuid(uuid1);
-        PersonManager.getInstance().addNurse(patient1);
-        int count = PersonManager.getInstance().countNurse(new BasicDBObject("uuid", uuid1.toString()));
-        assertEquals(1, count);
-
-        // Add patient 2 with same uuid as patient1.
-        Nurse patient2 = new Nurse("Patient2");
-        patient2.setUuid(uuid1);
-        PersonManager.getInstance().addNurse(patient2);
-        count = PersonManager.getInstance().countNurse(new BasicDBObject("uuid", uuid1.toString()));
-        assertEquals(1, count);
-
-        // Add patient 3 with different id.
-        Nurse patient3 = new Nurse("Patient3");
-        patient3.setUuid(uuid2);
-        PersonManager.getInstance().addNurse(patient3);
-
-        count = PersonManager.getInstance().countNurse(new BasicDBObject("uuid", uuid1.toString()));
-        assertEquals(1, count);
-        count = PersonManager.getInstance().countNurse(new BasicDBObject("uuid", uuid2.toString()));
-        assertEquals(1, count);
+        Nurse nurse = new Nurse("Patient1");
+        nurse.setUuid(uuid1);
+        assertFalse(PersonManager.getInstance().nurseExists(nurse.getUuid()));
+        PersonManager.getInstance().addNurse(nurse);
+        assertTrue(PersonManager.getInstance().nurseExists(nurse.getUuid()));
+        PersonManager.getInstance().addNurse(nurse);
     }
 
 
