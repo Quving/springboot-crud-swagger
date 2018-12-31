faf95e51a9b92d9ffcaaa5186c0b2443a870ec8e
Vinh Ngu <vinh-ngu@hotmail.com>
2018-12-31 15:20:59 +0100

    Remove toJson(), fromJso().

--- src/main/java/asclepius/Nurse.java
+++ src/main/java/asclepius/Nurse.java
@@ -1,28 +1,10 @@
 package asclepius;
 
-import org.json.JSONObject;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "nurses")
 public class Nurse extends Person {
     public Nurse(String name) {
         super(name);
-    }
-
-    public String toJson() {
-        JSONObject jo = new JSONObject();
-        jo.put("type", _type);
-        jo.put("name", _name);
-        jo.put("uuid", _uuid);
-        jo.put("location", _location.toJson());
-
-        return jo.toString();
-    }
-
-    public static Nurse fromJson(String json) {
-        JSONObject jo = new JSONObject(json);
-        Nurse nurse = new Nurse(jo.getString("name"));
-        nurse.setLocation(Location.fromJson(jo.getString("location")));
-        return nurse;
     }
 }
--- src/main/java/asclepius/Patient.java
+++ src/main/java/asclepius/Patient.java
@@ -1,29 +1,10 @@
 package asclepius;
 
-import org.json.JSONObject;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "patients")
 public class Patient extends Person {
     public Patient(String name) {
         super(name);
-    }
-
-    public String toJson() {
-        JSONObject jo = new JSONObject();
-        jo.put("type", _type);
-        jo.put("name", _name);
-        jo.put("uuid", _uuid);
-        jo.put("location", _location.toJson());
-
-        return jo.toString();
-    }
-
-    public static Patient fromJson(String json) {
-        JSONObject jo = new JSONObject(json);
-        Patient patient = new Patient(jo.getString("name"));
-        patient.setLocation(Location.fromJson(jo.getString("location")));
-
-        return patient;
     }
 }
