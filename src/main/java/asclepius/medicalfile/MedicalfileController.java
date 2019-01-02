f3514599dce527edccb10c0714ffe8596067c5cf
Vinh Ngu - Potato <Vinh@HOOU17007.local>
2019-01-02 17:37:02 +0100

    Fix parameter name.

--- src/main/java/asclepius/medicalfile/MedicalfileController.java
+++ src/main/java/asclepius/medicalfile/MedicalfileController.java
@@ -1,5 +1,7 @@
 package asclepius.medicalfile;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -8,15 +10,17 @@ import java.util.List;
 @RestController
 @RequestMapping("/medicalfile")
 public class MedicalfileController {
+    private Logger logger = LoggerFactory.getLogger(MedicalfileController.class);
     @Autowired
     private MedicalfileService _medicalfileService;
 
     @RequestMapping(method = RequestMethod.POST, value = "/add")
-    public void addMedicalfile(@RequestBody Medicalfile person) {
-        _medicalfileService.addMedicalfile(person);
+    public void addMedicalfile(@RequestBody Medicalfile medicalfile) {
+        logger.info(medicalfile.getNurseUuid());
+        _medicalfileService.addMedicalfile(medicalfile);
     }
 
-    @RequestMapping(method = RequestMethod.GET, value = "/get/{id}")
+    @RequestMapping(method = RequestMethod.GET, value = "/get/{uuid}")
     public void getMedicalfile(@PathVariable String uuid) {
         _medicalfileService.getMedicalfile(uuid);
     }
@@ -27,8 +31,8 @@ public class MedicalfileController {
     }
 
     @RequestMapping(method = RequestMethod.PUT, value = "/update/{id}")
-    public void updateMedicalfile(@RequestBody Medicalfile person, @PathVariable String id) {
-        _medicalfileService.updateMedicalfile(id, person);
+    public void updateMedicalfile(@RequestBody Medicalfile medicalfile, @PathVariable String id) {
+        _medicalfileService.updateMedicalfile(id, medicalfile);
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "/all")
