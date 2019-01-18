1075dea56eb6f83e478b1fabbfd60337b0e1ecb7
Vinh Ngu <vinh-ngu@hotmail.com>
2019-01-18 15:17:28 +0100

    Add unimplemented MedicalfileTest.java.

---
+++ src/test/java/asclepius/medicalfile/MedicalfileTest.java
@@ -0,0 +1,18 @@
+package asclepius.medicalfile;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MedicalfileTest {
+
+    @Test
+    public void addNurse() {
+
+    }
+
+    @Test
+    public void removeNurse() {
+
+    }
+}
